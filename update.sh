3c0cbe9d8570c1f1699cce5948a3971c83715296
dacha vineeth kumar <vineeth25d60@gmail.com>
2020-04-25 13:10:44 +0530

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -ue
+. functions.sh
+
+cd "$(cd "${0%/*}" && pwd -P)"
+
+IFS=' ' read -ra versions <<< "$(get_versions .)"
+
+url='https://github.com/RocketChat/Rocket.Chat.git'
+
+get_latest() {
+  git ls-remote --tags $url \
+      | awk -F 'refs/tags/' '
+        $2 ~ /^v?[0-9]/ {
+          gsub(/^v|\^.*/, "", $2);
+          print $2;
+        }
+      ' \
+      | sort -uV \
+      | grep -vE -- '-(rc|alpha|beta)' \
+      | grep "^${1}" \
+      | tail -1
+}
+
+for version in "${versions[@]}"; do
+  if [ -f "${version}/Dockerfile" ]; then
+    sed -ri 's/^(ENV RC_VERSION) .*/\1 '"$(get_latest "${version}")"'/;' "${version}/Dockerfile"
+  fi
+done;
